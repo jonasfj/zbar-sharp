14dd161c0f7f521c2cc53ae58566126746f76960
Jonas Finnemann Jensen <jopsen@gmail.com>
2010-12-20 23:15:43 +0100

    Cleanup and QR code support

--- libzbar-cil/ZBar.cs
+++ libzbar-cil/ZBar.cs
@@ -1,3 +1,25 @@
+/*------------------------------------------------------------------------
+ *  Copyright 2009 (c) Brandon McCaig <jopsen@gmail.com>
+ *
+ *  This file is part of the ZBar CIL Wrapper.
+ *
+ *  The ZBar CIL Wrapper is free software; you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser Public License as
+ *  published by the Free Software Foundation; either version 2.1 of
+ *  the License, or (at your option) any later version.
+ *
+ *  The ZBar CIL Wrapper is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ *  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser Public License
+ *  along with the ZBar CIL Wrapper; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ *  Boston, MA  02110-1301  USA
+ * 
+ *------------------------------------------------------------------------*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
